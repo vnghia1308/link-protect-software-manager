1df9ae5d5a1f692a482a1e10793a1b9a60744eb4
Vy Nghia <dora.nghia@gmail.com>
2019-01-20 19:07:08 +0700

    Update this.config.php

--- web-api/this.config.php
+++ web-api/this.config.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 require_once "../system/config.php";
-require_once '../system/class/PseudoCrypt.php';
+require_once '../system/class/ProtectClass.php';
 
 $q1 = mysqli_query($db, "select * from api_server where 1");
 $s = mysqli_fetch_array($q1);
